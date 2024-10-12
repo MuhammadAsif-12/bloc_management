1321d75358d7d0b85161cde321949e1afe997c81
Muhammad Asif <m.asif.addi@gmail.com>
2024-10-12 20:20:40 +0500

    added Welcome UI .

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:bloc_management/bloc/internet_bloc/internet_bloc.dart';
 import 'package:bloc_management/screens/home_screen.dart';
 import 'package:bloc_management/screens/sign_in/sign_in_page.dart';
+import 'package:bloc_management/screens/sign_in/welcome_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -24,7 +25,7 @@ class MyApp extends StatelessWidget {
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
-        home: const SignIn(),
+        home: const WelcomePage(),
       ),
     );
   }
